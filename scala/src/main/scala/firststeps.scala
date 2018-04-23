42d3092c70df70f040aa46f4b4b7afe05055d760
Simon Andersen <anders_s@epita.fr>
2018-04-23 16:42:18 +0200

    [Scala] last, nth, reverse and compress lists

--- scala/src/main/scala/firststeps.scala
+++ scala/src/main/scala/firststeps.scala
@@ -1,4 +1,4 @@
-object Tp{
+object Tp1{
   case class Majeure(name: String, surname: String="test")
 
   def tupleToCase (tuple: (String, String)) : Majeure = Majeure(tuple._1, tuple._2)
@@ -22,16 +22,58 @@ object Tp{
     }
     loop()
   }
+
+  def test(): Unit = {
+    val scia = tupleToCase(("SCIA", "ML"))
+    println(scia)
+
+    applyOnFirstTen(even)
+    println(list10First(even))
+
+    println(List(Majeure("SCIA"), Majeure("GITM")).map(x => x.name))
+
+    println(Stream.from(0).filter(even).take(10).toList)
+  }
+}
+
+object Tp2 {
+  def last(list: List[Int]): Int = list match {
+    case e :: Nil => e
+    case _ :: l => last(l)
+  }
+
+  def nth(n: Int, list: List[Int]): Int = (n, list) match {
+    case (0, e :: _) => e
+    case (x, _ :: l) => nth(x - 1, l)
+    case (_, Nil) => -1
+  }
+
+  def reverse(list: List[Int]): List[Int] = list match {
+    case Nil => Nil
+    case e :: l => reverse(l) ::: List(e)
+  }
+
+  def compress(list: List[Symbol]): List[Symbol] = list match {
+    case Nil => Nil
+    case a :: b :: l if a == b => compress(a::l)
+    case a :: l => a :: compress(l)
+  }
+
+  def test(): Unit = {
+    println(last(List(1, 1, 2, 3, 5, 8)))
+    println(last(List(1, 2, 3)))
+
+    println(nth(2, List(1, 1, 2, 3, 5, 8)))
+    println(nth(10, List(1, 2, 3)))
+
+    println(reverse(List(1, 1, 2, 3, 5, 8)))
+
+    println(compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)))
+  }
 }
 
 object Main extends App {
-  val scia = Tp.tupleToCase(("SCIA", "ML"))
-  println(scia)
+  //Tp1.test()
 
-  Tp.applyOnFirstTen(Tp.even)
-  println(Tp.list10First(Tp.even))
-
-  println(List(Tp.Majeure("SCIA"), Tp.Majeure("GITM")).map(x => x.name))
-
-  println(Stream.from(0).filter(Tp.even).take(10).toList)
+  Tp2.test()
 }
