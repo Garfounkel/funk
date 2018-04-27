f4feb7e57fd49ac560e87aa68a2245373acab633
Simon Andersen <anders_s@epita.fr>
2018-04-27 15:39:59 +0200

    [Scala] done until decode

--- scala/src/main/scala/firststeps.scala
+++ scala/src/main/scala/firststeps.scala
@@ -37,38 +37,63 @@ object Tp1{
 }
 
 object Tp2 {
-  def last(list: List[Int]): Int = list match {
+  def last(list: List[Any]): Any = list match {
+    case Nil => ???
     case e :: Nil => e
     case _ :: l => last(l)
   }
 
-  def nth(n: Int, list: List[Int]): Int = (n, list) match {
+  def nth(n: Int, list: List[Any]): Any= (n, list) match {
     case (0, e :: _) => e
     case (x, _ :: l) => nth(x - 1, l)
-    case (_, Nil) => -1
+    case (_, Nil) => throw new IndexOutOfBoundsException
   }
 
-  def reverse(list: List[Int]): List[Int] = list match {
+  def reverse[A](list: List[A]): List[A] = list match {
     case Nil => Nil
     case e :: l => reverse(l) ::: List(e)
   }
 
-  def compress(list: List[Symbol]): List[Symbol] = list match {
+  def compress(list: List[Any]): List[Any] = list match {
     case Nil => Nil
     case a :: b :: l if a == b => compress(a::l)
     case a :: l => a :: compress(l)
   }
 
+  def compress_foldLeft1(list: List[Any]): List[Any] = {
+    list.tail.foldLeft(List(list.head)) { (acc, e) => if (acc.head != e) e :: acc else acc}.reverse
+  }
+
+  def encode(list: List[Any]): List[(Int, Any)] = list.foldLeft(List[(Int, Any)]()) {
+    case (Nil, elt) => List((1, elt))
+    case ((n, e)::tail, elt) if e == elt => (n + 1, elt) :: tail
+    case (acc, elt) => (1, elt) :: acc
+  }.reverse
+
+  def decode(list: List[(Int, Any)]): List[Any] = list.flatMap(e => List.fill(e._1)(e._2))
+
+  def decode2(list: List[(Int, Any)]): List[Any] = list.flatMap(e => (1 to e._1).map(_ => e._2))
+
   def test(): Unit = {
     println(last(List(1, 1, 2, 3, 5, 8)))
     println(last(List(1, 2, 3)))
+    println(List(1, 1, 2, 3, 7, 2, 8).takeRight(1).head)
+    println(List(1, 1, 2, 3, 7, 2, 8).last)
 
     println(nth(2, List(1, 1, 2, 3, 5, 8)))
-    println(nth(10, List(1, 2, 3)))
+    println(List(1, 2, 3)(2))
 
     println(reverse(List(1, 1, 2, 3, 5, 8)))
+    println(List(1, 1, 2, 3, 5, 8).reverse)
 
     println(compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)))
+    val l = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+    println(compress_foldLeft1(l))
+
+    println(encode(l))
+
+    println(decode(encode(l)))
+    println(decode2(encode(l)))
   }
 }
 
---
+++ scala/src/main/scala/test-main/main.scala
@@ -0,0 +1,5 @@
+object MainTest {
+  def main(args: Array[String]): Unit = {
+    println(args(0))
+  }
+}
